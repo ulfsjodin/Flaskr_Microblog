3a4b38e2d7942724143c960e9686186ce63f4b6b
ulfsjodin <sjodin.ulf@gmail.com>
2018-07-28 23:23:44 +0200

    preparing to get the project installable

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
 #setup.py
+
+from setuptools import find_packages, setup
+
+setup(
+    name='flaskr',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_dta=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+    ],
+)
